f01986d5dd2c867d3bee3791c120b186fa865533
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-10 16:35:33 +0300

    Fetch API

--- app/main.go
+++ app/main.go
@@ -44,20 +44,18 @@ func handleRequest(conn net.Conn) {
 			no need to check message_size like before copy(response[:4], req[:4])
 			simple count our response size instead
 		*/
-		conn.Write([]byte{0, 0, 0, 19})
+		conn.Write([]byte{0, 0, 0, 12})
 
 		response := make([]byte, 19)
-		copy(response[:4], req[8:12])     // correlation_id param - 4 bytes
-		copy(response[4:], version_error) // error_code (represents no error in this case) - 2 bytes
-		response[6] = 1                   // Number of API keys - 1 byte
-
+		copy(response[:4], req[8:12])      // correlation_id param - 4 bytes
+		copy(response[4:], version_error)  // error_code (represents no error in this case) - 2 bytes
+		response[6] = 1                    // Number of API keys - 1 byte
 		copy(response[7:], []byte{0, 1})   // API Key api_version - 2 bytes
 		copy(response[9:], []byte{0, 3})   // min version - 2 bytes
 		copy(response[11:], []byte{0, 16}) // max version - 2 bytes
-
-		response[13] = 0                        // _tagged_fields - 1 byte
-		copy(response[14:], []byte{0, 0, 0, 0}) // throttle time - 4 bytes
-		response[18] = 0                        // 1 byte
+		//response[13] = 0                        // _tagged_fields - 1 byte
+		//copy(response[14:], []byte{0, 0, 0, 0}) // throttle time - 4 bytes
+		//response[18] = 0                        // 1 byte
 
 		_, err := conn.Write(response)
 
