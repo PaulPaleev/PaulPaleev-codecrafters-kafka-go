5517710dabc9420303d7fc6fb0d2e8608595f750
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-10 14:32:49 +0300

    fixed

--- app/main.go
+++ app/main.go
@@ -38,18 +38,16 @@ func handleRequest(conn net.Conn) {
 
 	response := make([]byte, 19)
 
-	copy(response, req[:4]) // message_size param
-
+	//copy(response, req[:4])                      // message_size param
+	copy(response[:4], req[8:12])                // correlation_id param
 	copy(response[4:], version_error)            // error_code (represents no error in this case)
 	response[6] = 2                              // Number of API keys
 	copy(response[7:], req[6:8])                 // api_version
-	binary.BigEndian.PutUint16(response[9:], 3)  //             min version
-	binary.BigEndian.PutUint16(response[11:], 4) //             max version
+	binary.BigEndian.PutUint16(response[9:], 3)  // min version
+	binary.BigEndian.PutUint16(response[11:], 4) // max version
 	response[13] = 0                             // _tagged_fields
 	binary.BigEndian.PutUint32(response[14:], 0) // throttle time
 	response[18] = 0
-
-	//copy(response[4:8], req[8:12])    // correlation_id param
 
 	conn.Write(response)
 }
