e4c6a54098b3cf410617aedecc225158c34d1dc8
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-10 16:06:47 +0300

    refactored concurency

--- app/main.go
+++ app/main.go
@@ -22,48 +22,45 @@ func main() {
 			os.Exit(1)
 		}
 
-		go handleRequest(conn)
-		// handleRequest(conn)
+		handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
-	//defer conn.Close()
+	defer conn.Close()
 
-	//run := true
+	for {
+		req := make([]byte, 1024)
+		conn.Read(req)
+		ver := binary.BigEndian.Uint16(req[6:8])
+		var version_error []byte
+		switch ver {
+		case 0, 1, 2, 3, 4:
+			version_error = []byte{0, 0}
+		default:
+			version_error = []byte{0, 35}
+		}
+		/*
+			no need to check message_size like before copy(response[:4], req[:4])
+			simple count our response size instead
+		*/
+		conn.Write([]byte{0, 0, 0, 19})
 
-	//for run {
-	req := make([]byte, 1024)
-	conn.Read(req)
-	ver := binary.BigEndian.Uint16(req[6:8])
-	var version_error []byte
-	switch ver {
-	case 0, 1, 2, 3, 4:
-		version_error = []byte{0, 0}
-	default:
-		version_error = []byte{0, 35}
+		response := make([]byte, 19)
+		copy(response[:4], req[8:12])           // correlation_id param - 4 bytes
+		copy(response[4:], version_error)       // error_code (represents no error in this case) - 2 bytes
+		response[6] = 2                         // Number of API keys - 1 byte
+		copy(response[7:], []byte{0, 18})       // API Key api_version - 2 bytes
+		copy(response[9:], []byte{0, 3})        // min version - 2 bytes
+		copy(response[11:], []byte{0, 4})       // max version - 2 bytes
+		response[13] = 0                        // _tagged_fields - 1 byte
+		copy(response[14:], []byte{0, 0, 0, 0}) // throttle time - 4 bytes
+		response[18] = 0                        // 1 byte
+
+		_, err := conn.Write(response)
+
+		if err != nil {
+			break
+		}
 	}
-	/*
-		no need to check message_size like before copy(response[:4], req[:4])
-		simple count our response size instead
-	*/
-	conn.Write([]byte{0, 0, 0, 19})
-
-	response := make([]byte, 19)
-	copy(response[:4], req[8:12])           // correlation_id param - 4 bytes
-	copy(response[4:], version_error)       // error_code (represents no error in this case) - 2 bytes
-	response[6] = 2                         // Number of API keys - 1 byte
-	copy(response[7:], []byte{0, 18})       // API Key api_version - 2 bytes
-	copy(response[9:], []byte{0, 3})        // min version - 2 bytes
-	copy(response[11:], []byte{0, 4})       // max version - 2 bytes
-	response[13] = 0                        // _tagged_fields - 1 byte
-	copy(response[14:], []byte{0, 0, 0, 0}) // throttle time - 4 bytes
-	response[18] = 0                        // 1 byte
-
-	_, err := conn.Write(response)
-
-	if err != nil {
-		//run = false
-	}
-	//}
 }
