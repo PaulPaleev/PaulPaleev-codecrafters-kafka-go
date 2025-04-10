7fdf2640f4ec3f6b7aecb2b76cf4271ddf98194a
Pavel <pavel.panteleev@vrpconsulting.com>
2025-04-10 14:13:39 +0300

    fixed response

--- app/main.go
+++ app/main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
@@ -20,7 +19,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	//var correlation_id in32 = 7
 	handleRequest(conn)
 }
 
@@ -28,27 +26,12 @@ func handleRequest(conn net.Conn) {
 	defer conn.Close()
 	req := make([]byte, 1024)
 	conn.Read(req)
-	strReq := string(req)
 	fmt.Println("req: ", req)
-	fmt.Println("strReq: ", strReq)
 
-	response := make([]byte, 10)
-	copy(response, req[0:4]) // message_size param
-	/*
-		request example:
-		so we start copy elements from 8 since it's the first element of correlation_id
-		00 00 00 23  // message_size:        35
-		00 12        // request_api_key:     18
-		00 04        // request_api_version: 4
-		6f 7f c6 61  // correlation_id:      1870644833
-	*/
-	copy(response[4:8], req[8:12])   // [4:8] bc we have [0:4] as message_size param, [8:12] correlation_id param in the request
-	copy(response[8:], []byte{0, 0}) // error_code 35 (we have only 2 elements to fill)
-	/*
-		response example:
-		00 00 00 00  // message_size:   0 (any value works)
-		4f 74 d2 8b  // correlation_id: 1333056139
-		00 23        // error_code:     35
-	*/
+	response := make([]byte, 19)
+	copy(response, req[0:4])       // message_size param
+	copy(response, req[6:8])       // api_version
+	copy(response[4:8], req[8:12]) // correlation_id param
+	copy(response[8:], []byte{})   // error_code (represents no error in this case)
 	conn.Write(response)
 }
